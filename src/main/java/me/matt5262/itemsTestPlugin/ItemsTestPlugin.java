32d9d8e6274ee07afe8483bd34977c0177f976ef
Matti Bruun <mattibruun2000@gmail.com>
2025-09-15 13:50:36 +0200

    No message

---
+++ src/main/java/me/matt5262/itemsTestPlugin/ItemsTestPlugin.java
@@ -0,0 +1,17 @@
+package me.matt5262.itemsTestPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemsTestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
